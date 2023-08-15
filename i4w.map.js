8b9660843d55b37adfec60f8814b3896d5bf7779
KBismark <bismarkkwabenayamoah@gmail.com>
2023-08-15 18:23:34 +0000

    Changed package configurations

--- i4w.map.js
+++ i4w.map.js
@@ -2,5 +2,5 @@
 const path = require('path');
 module.exports =() => (
 {
-  "/modules/honeybee-client@0.1.1": path.join(`${__dirname}/lib`,"/index.js")
+  "/modules/honeybee-client@0.1.0": path.join(`${__dirname}/lib`,"/index.js")
 })
